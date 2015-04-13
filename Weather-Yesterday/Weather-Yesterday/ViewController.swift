a9c975cca2452f982874903360b88dd0e3576839
Christian Yenko <chris@jivehealth.com>
2015-04-13 03:56:32 -0500

    Implemented Weather API Call + Barebones Display

--- Weather-Yesterday/Weather-Yesterday/ViewController.swift
+++ Weather-Yesterday/Weather-Yesterday/ViewController.swift
@@ -28,14 +28,14 @@ class ViewController: UIViewController, NSURLConnectionDelegate{
         let weatherKey = configDict!.valueForKey("OpenWeatherAPI")?.valueForKey("APIKey") as? String
         
         log("GET weather for zip " + zipcode)
-        //Dynamically change the urlStr to user input
-        var urlStr = "http://headers.jsontest.com/"
+        //Dynamically change the urlStr to user inputted zip code
+        var urlStr = "http://api.openweathermap.org/data/2.5/weather?zip=60201,us&APPID="+weatherKey!
+        log(urlStr)
         var url: NSURL = NSURL(string: urlStr)!
         var connection: NSURLConnection = NSURLConnection(request: NSURLRequest(URL:url),delegate:self,startImmediately:false)!
         connection.start()
         log("Started connection.")
         
-        //Using SwiftyJSON b/c parsing JSON in native swift is a pain
         
     }
     
@@ -44,8 +44,26 @@ class ViewController: UIViewController, NSURLConnectionDelegate{
         self.inData.appendData(conData)
     }
     func connectionDidFinishLoading(connection:NSURLConnection!){
+        log("Connection terminated.")
         var result: NSDictionary = NSJSONSerialization.JSONObjectWithData(inData, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
-        println(result)
+        let json = JSON(data:inData)
+        //Using SwiftyJSON b/c parsing JSON in native swift is a pain
+
+        println(json["main"]["temp"])
+
+        if let weatherKelvin = json["main"]["temp"].double{
+            //If the value exists, grab it and update the label
+            log("WEATHER: \(weatherKelvin)")
+            lblTitle.text = "Today's Weather: \(weatherKelvin-273.15) Celsius"
+        }
+        else{
+            lblTitle.text = "Weather not found!"
+        }
+        
+    }
+     func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        log("ERROR")
+        println(error)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,7 +75,6 @@ class ViewController: UIViewController, NSURLConnectionDelegate{
         // Do any additional setup after loading the view, typically from a nib.
         var alert = UIAlertController(title:"Test",message:"Hi Brittany!",preferredStyle:UIAlertControllerStyle.Alert)
         alert.addAction(UIAlertAction(title:"Click Me!",style:UIAlertActionStyle.Default,handler:nil))
-        log("About to calll")
         getWeather("60201")
         self.presentViewController(alert, animated: true, completion: nil)
         //Load weather for today and display in label
